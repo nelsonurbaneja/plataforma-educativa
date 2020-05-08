3ffe31cfd10b0dbefed842b65aabff013b194395
Nelson Urbaneja <nelsongregoriourbaneja@hotmail.com>
2020-05-08 00:09:04 -0400

    Se instala node-sass y se importa el archivo styles.scss en index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './components/App'
+import './styles/styles.scss'
 
 const root =  document.getElementById('root')
 ReactDOM.render(<App/>, root)
