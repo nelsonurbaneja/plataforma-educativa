93fb53cb42957de55322e3a8a0b26caf3ae0c708
Nelson Urbaneja <nelsongregoriourbaneja@hotmail.com>
2020-05-09 00:23:50 -0400

    Se crea el Componente Card, que se usara en el Grid de Especilidades y el Grid de Cursos

---
+++ src/components/Organisms/Card.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+const Card = ({cardID, picture, name, path}) => (
+  <article className="card s-border">
+    <div className="img-container s-ratio-16-9 s-mb-2">
+      <Link to={`/${path}/${cardID}`}>
+        <img src={picture} alt={name} />
+      </Link>
+    </div>
+    <div className="card__data s-pxy-2">
+      <h3 className="t4 center">{name}</h3>
+    </div>
+  </article>
+)
+
+export default Card
