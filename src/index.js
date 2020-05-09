44c9b420896b347da503b901b291ed6dc64dea8d
Nelson Urbaneja <nelsongregoriourbaneja@hotmail.com>
2020-05-08 20:16:55 -0400

    Se agrega el Provider a nuestra app

--- src/index.js
+++ src/index.js
@@ -2,6 +2,16 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './components/App'
 import './styles/styles.scss'
+import { Provider } from 'react-redux'
+import store from './redux/store'
+
+import { getAllSpecialities } from './redux/actionCreators'
+
+store.dispatch(getAllSpecialities())
 
 const root =  document.getElementById('root')
-ReactDOM.render(<App/>, root)
+ReactDOM.render(
+  <Provider store={store}>
+    <App/>
+  </Provider>,  root
+)
